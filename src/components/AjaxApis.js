e604afa9f7551e3c93a4b0be3677ac8d18b3e53f
Omar <omar.gbet@gmail.com>
2021-03-11 19:47:05 -0600

    first commit

---
+++ src/components/AjaxApis.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Pokemon(props) {
+    return(
+        <figure>
+
+        </figure>
+    )
+}
+
+export default class AjaxApi extends Component
